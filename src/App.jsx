8e2bac1fe7c659ea63dec9b0cabe5f6bc5ab6cd7
yelgon <yelgon36@gmail.com>
2020-06-29 22:04:15 -0400

    useEffect test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://www.reddit.com/r/reactjs.json");
+      const json = await response.json();
+      setPosts(json.data.children.map((c) => c.data));
+    }
+    fetchData();
+  }, [setPosts]);
+  console.log(posts);
+
   return (
     <div>
-      <h2>Hello world!</h2>
+      <ol>
+        {posts.map((post, idx) => (
+          <li key={idx}>{post.title},</li>
+        ))}
+      </ol>
     </div>
   );
 }
